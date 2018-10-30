0f0bf8f8b4ca944f32fd6069e126df269f50246c
U-RLKF-001\admin <RLKF-001+admin@RLKF-001.chinabond.com.cn>
2018-10-30 17:10:08 +0800

    add brower header and time interval

--- getBookScore.py
+++ getBookScore.py
@@ -2,7 +2,10 @@ import urllib.request
 import urllib.parse
 from bs4 import BeautifulSoup
 import os
+from fake_useragent import UserAgent
+import time,random
 
+ua = UserAgent()
 def getTags():
     url='https://book.douban.com/tag/?view=cloud'
     response = urllib.request.urlopen(url)
@@ -12,6 +15,14 @@ def getTags():
     for book in soup("td"):
         tags.append(book.a.get_text())
     return tags
+def visitUsingHeader(url):
+    header={"User-Agent":ua.random}#using random headers
+    req=urllib.request.Request(url=url,headers=headers)
+    response=urllib.request.urlopen(req)
+    page=response.read().decode('utf-8')
+    soup=BeautifulSoup(page,'html.parser')
+    return soup
+
 f=open('book.csv','w')
 tags=getTags()
 for tag in tags[1:]:
@@ -20,9 +31,8 @@ for tag in tags[1:]:
     for start in range(0,40,20):
         url='https://book.douban.com/tag/%s?start=%d&type=%s'%(subject,start,ty)
         print(url)
-        response = urllib.request.urlopen(url)
-        page = response.read().decode('utf-8')
-        soup=BeautifulSoup(page,'html.parser')
+        time.sleep(5)
+        soup=visitUsingHeader(url)
         for book in soup("div",class_="info"):
             if book==None or book.find("span",class_="rating_nums")==None or book.find("span",class_="rating_nums").get_text()=='': continue
             score=float(book.find("span",class_="rating_nums").get_text())
